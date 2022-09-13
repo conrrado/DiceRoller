be24e34f38c2f144b1fba138dbee59ca46f9c13d
Conrrado Kantoviz Francisco Camacho <conrrado@motorola.com>
2022-09-12 22:25:32 -0300

    Dice Roller project
    Project based on Google's Codelabs
    Unit 1: Kotlin basics

---
+++ app/src/main/java/com/conrradocamacho/diceroller/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.conrradocamacho.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDiceAndUpdateView() }
+    }
+
+    private fun rollDiceAndUpdateView() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
